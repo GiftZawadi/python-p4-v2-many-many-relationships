de521e75b4efe79a22298fc8ced70683cda9b3c6
Gift <gzomandi07@gmail.com>
2024-07-06 17:12:09 +0300

    initial commit

--- server/models.py
+++ server/models.py
@@ -8,8 +8,7 @@ metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
-db = SQLAlchemy(metadata=metadata)
-
+db = SQLAlchemy(metadata=metadata) 
 
 class Employee(db.Model):
     __tablename__ = 'employees'
